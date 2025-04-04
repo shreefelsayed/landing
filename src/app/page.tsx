fe2a9ea857f0d996df6611a1f0c9fc84b2faaef2
Shreif El Sayed <64437103+shreefelsayed@users.noreply.github.com>
2025-04-04 06:45:22 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { PaymentOptionsSection } from "../components/payment-options-section"
 import { CTASection } from "../components/cta-section"
 import { FAQSection } from "../components/faq-section"
 import { Footer } from "../components/footer"
-
+///
 export default function Home() {
   return (
     <div className="">
